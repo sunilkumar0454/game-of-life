8d74e9bfdafcddd327c6d37d0922c5436d12c80c
sunilkumar0454 <sunilrb2b@gmail.com>
2022-06-02 22:43:13 +0530

    Deleted : Changes after the code saved

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -118,15 +118,7 @@ public class GameController {
         return randomGenerator;
     }
    
-    /* HelloWorld.java
- */
-
-public class HelloWorld
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
+   
 
 }
  
