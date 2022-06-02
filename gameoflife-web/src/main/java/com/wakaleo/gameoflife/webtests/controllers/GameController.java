fa75b46745c7ea25ea7590efb00472ac508b2e84
sunilkumar0454 <sunilrb2b@gmail.com>
2022-06-02 22:40:45 +0530

    Addded : New changes to the code for test

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -118,6 +118,15 @@ public class GameController {
         return randomGenerator;
     }
    
+    /* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
 
 }
  
